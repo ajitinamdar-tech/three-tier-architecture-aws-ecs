e9f17672aaed81e34d70a4c8b4b7bc62b6fa1b5b
Ajit Inamdar <ajitinamdar.tech@gmail.com>
2025-05-15 15:20:34 +0530

    Making changes to Deploy ECR step

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "main" {
   allocated_storage   = var.db_allocated_storage
   storage_type        = var.db_storage_type
   db_name             = var.db_name
-  username            = var.db_username
+  username            = var.db_user
   password            = var.db_password
   skip_final_snapshot = true
 
